b5094b27c9bcc12111da771839e90d8676907624
charu <charu@2kxconsulting.com>
2024-02-08 20:31:02 +0530

    fixed bugs validation related

--- src/components/Heirarchy/CategoryCreation.jsx
+++ src/components/Heirarchy/CategoryCreation.jsx
@@ -16,7 +16,7 @@ import { FormControlLabel, List, ListItem } from '@mui/material';
 import { ListItemText } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
-const CategoryModal = ({ updateCategory, addNewCategory,selectedButton, categoryObj, onSave, onClose }) => {
+const CategoryModal = ({ updateCategory, addNewCategory, selectedButton, categoryObj, onSave, onClose }) => {
     const [category, setCategory] = useState(categoryObj.name);
     const [categoryStatus, setCategoryStatus] = useState(null);
     const [isOpen, setIsOpen] = useState(true)
@@ -38,7 +38,7 @@ const CategoryModal = ({ updateCategory, addNewCategory,selectedButton, category
         // });
 
         // Clear fields and close modal
-        
+
         setCategory('');
         setCategoryStatus(null);
         setSubcategory('');
@@ -66,33 +66,62 @@ const CategoryModal = ({ updateCategory, addNewCategory,selectedButton, category
 
     const setSubcategoryStatus = (value) => {
         let subCategoryObj = {
-            "name" : subcategory,
-            "reports" : reports,
+            "name": subcategory,
+            "reports": reports,
             exact: true, to: '/report/', iconClassName: 'bi bi-file-earmark', component: 'Admin',
-            "isOpen" : false
+            "isOpen": false
         }
 
         let categoryObject = {
-            "name" : category,
-            "subCategories" : [subCategoryObj],
+            "name": category,
+            "subCategories": subcategory == "" ? [] : [subCategoryObj],
             exact: true, to: '/report/*', iconClassName: 'bi bi-file-earmark', component: 'Report',
-            "reports" : []
+            "reports": subcategory == "" ? reports : []
         }
 
         let subCategoryObjCopy = categoryObj?.subCategories ? categoryObj.subCategories : [];
         let reportsCopy = categoryObj?.reports ? categoryObj.reports : [];
+        // selectedButton === "report" && reportsCopy?.push(...reports)
+        let error = ""
+        if (selectedButton === "report") {
+            // Filter out reports that already exist in the reportsCopy
+            const newReports = reports.filter(report => !reportsCopy.some(existingReport => existingReport.id === report.id));
 
-        // console.log("con",  categoryObj,categoryObj?.subCategories,subCategoryObjCopy.push("hi"))
+            if (newReports.length == 0) {
 
-        if(selectedButton === "subCategory" && subCategoryObj?.name !== ""){
-            subCategoryObjCopy?.push(subCategoryObj)
+                error = "Report added already exist try adding new report!"
+                console.log("con1", newReports, error)
+
+            }
+            reportsCopy.push(...newReports);
         }
-        selectedButton === "report" && reportsCopy?.push(...reports)
+
+
+        // if (selectedButton === "subCategory" && subCategoryObj?.name !== "") {
+        //     subCategoryObjCopy?.push(subCategoryObj)
+
+        // }
+
+        if (selectedButton === "subCategory" && subCategoryObj?.name !== "") {
+            // Check if subcategory with the same name already exists
+            const existingSubCategory = subCategoryObjCopy.find(subCategory => subCategory.name === subCategoryObj.name);
+
+            if (!existingSubCategory) {
+                subCategoryObjCopy.push(subCategoryObj);
+            } else {
+                error = `Subcategory with name '${subCategoryObj.name}' already exists.`
+                console.log(`Subcategory with name '${subCategoryObj.name}' already exists.`);
+            }
+        }
+
+        categoryObj = { ...categoryObj, subCategories: subCategoryObjCopy, reports: reportsCopy, error: error }
+        console.log("con", categoryObj)
+
+
         selectedButton == "category" && category != undefined && addNewCategory(categoryObject)
-        
-        
-        categoryObj= {...categoryObj, subCategories: subCategoryObjCopy, reports: reportsCopy}
-        value === "correct" && updateCategory(categoryObj)
+
+
+        updateCategory(categoryObj)
 
     }
 
@@ -248,68 +277,68 @@ const CategoryModal = ({ updateCategory, addNewCategory,selectedButton, category
                     </>
                 ))} */}
 
-<>
-                       {(selectedButton == "subCategory" || selectedButton == "category") && ( <div style={{ marginLeft: '16px', display: 'flex', alignItems: 'center', marginBottom: "16px" }}>
-                            <TextField
-                                label="Subcategory"
-                                value={subcategory}
-                                onChange={(e) => setSubcategory(e.target.value)}
-                                fullWidth
-                            />
-                            <IconButton onClick={() => setSubcategoryStatus('correct')}>
-                                <CheckCircleOutlineIcon />
-                            </IconButton>
-                            <IconButton onClick={() => setSubcategoryStatus('wrong')}>
-                                <HighlightOffIcon />
-                            </IconButton>
-                        </div>)}
+                <>
+                    {(selectedButton == "subCategory" || selectedButton == "category") && (<div style={{ marginLeft: '16px', display: 'flex', alignItems: 'center', marginBottom: "16px" }}>
+                        <TextField
+                            label="Subcategory"
+                            value={subcategory}
+                            onChange={(e) => setSubcategory(e.target.value)}
+                            fullWidth
+                        />
+                        <IconButton onClick={() => setSubcategoryStatus('correct')}>
+                            <CheckCircleOutlineIcon />
+                        </IconButton>
+                        <IconButton onClick={() => setSubcategoryStatus('wrong')}>
+                            <HighlightOffIcon />
+                        </IconButton>
+                    </div>)}
 
-                        <div style={{ marginBottom: '16px', display: 'flex', alignItems: 'center', marginLeft: '32px' }}>
+                    <div style={{ marginBottom: '16px', display: 'flex', alignItems: 'center', marginLeft: '32px' }}>
 
-                            <FormControl fullWidth>
-                                <InputLabel id="reports-label">Reports</InputLabel>
-                                <Select
-                                    labelId="reports-label"
-                                    id="reports"
-                                    multiple
-                                    value={reports}
-                                    onChange={(e) => setReports(e.target.value)}
-                                    input={<Input />}
-                                    renderValue={(selected) => `Selected ${selected.length} reports`}
-                                >
-                                    {updatedReports?.length > 0 && updatedReports?.map((report) => (
-                                        <MenuItem key={report} value={report}>
-                                            <Checkbox
-                                                checked={reports.includes(report)}
-                                                color="primary"
-                                            />
-                                            <ListItemText primary={report.name} />
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                            <IconButton onClick={() => setSubcategoryStatus('correct')}>
-                                <CheckCircleOutlineIcon />
-                            </IconButton>
-                            <IconButton onClick={() => setSubcategoryStatus('wrong')}>
-                                <HighlightOffIcon />
-                            </IconButton>
+                        <FormControl fullWidth>
+                            <InputLabel id="reports-label">Reports</InputLabel>
+                            <Select
+                                labelId="reports-label"
+                                id="reports"
+                                multiple
+                                value={reports}
+                                onChange={(e) => setReports(e.target.value)}
+                                input={<Input />}
+                                renderValue={(selected) => `Selected ${selected.length} reports`}
+                            >
+                                {updatedReports?.length > 0 && updatedReports?.map((report) => (
+                                    <MenuItem key={report} value={report}>
+                                        <Checkbox
+                                            checked={reports.includes(report)}
+                                            color="primary"
+                                        />
+                                        <ListItemText primary={report.name} />
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <IconButton onClick={() => setSubcategoryStatus('correct')}>
+                            <CheckCircleOutlineIcon />
+                        </IconButton>
+                        <IconButton onClick={() => setSubcategoryStatus('wrong')}>
+                            <HighlightOffIcon />
+                        </IconButton>
 
-                        </div>
+                    </div>
 
-                        {reports.length > 0 && (
-                            <Box sx={{ marginLeft: '32px', marginBottom: '16px', maxWidth: "93%" }}>
-                                <List >
-                                    {reports.map((report) => (
-                                        <ListItem key={report.id} sx={{ border: '1px solid #ccc', borderRadius: '4px', marginBottom: '8px', padding: '8px' }}>
-                                            <ListItemText primary={report.name} />
-                                        </ListItem>
-                                    ))}
-                                </List>
-                            </Box>
-                        )}
+                    {reports.length > 0 && (
+                        <Box sx={{ marginLeft: '32px', marginBottom: '16px', maxWidth: "93%" }}>
+                            <List >
+                                {reports.map((report) => (
+                                    <ListItem key={report.id} sx={{ border: '1px solid #ccc', borderRadius: '4px', marginBottom: '8px', padding: '8px' }}>
+                                        <ListItemText primary={report.name} />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Box>
+                    )}
 
-                    </>
+                </>
 
 
 
