6669fe183957a4463f9a825ccf657379b9f072c8
charu <charu@2kxconsulting.com>
2024-10-25 19:54:31 +0530

    fixed selreport issue and updated tiles layout

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -91,11 +91,11 @@ const MenuItem = (props) => {
                           // background: "white",
                           borderBottom: "1px solid grey",
                           backgroundColor:
-                            report.id == subcategoryReportSel || report?.name == window.location.pathname?.replace("%20"," ").split("/")?.slice(4).join("")
+                            report?.name == window.location.pathname?.replace("%20"," ").split("/")?.slice(4).join("")
                               ? "aquamarine"
                               : "",
                           fontWeight:
-                            report.id == subcategoryReportSel || report?.name == window.location.pathname?.replace("%20"," ").split("/")?.slice(4).join("")
+                            report?.name == window.location.pathname?.replace("%20"," ").split("/")?.slice(4).join("")
                               ? "bold"
                               : "inherit",
                           cursor: "pointer",
